2d7cfb74c1d4f084626de9d82009ca9bc297019e
Thiago Lima <thiagolimaes@gmail.com>
2018-03-30 16:54:33 -0300

    Alt

---
+++ src/SaperxPHP/Conector.php
@@ -0,0 +1,19 @@
+<?php namespace SaperxPHP;
+
+class Conector
+{
+    private $host;
+    private $usuario;
+    private $senha;
+    private $porta;
+    private $dbname;
+
+    public function __construct($host, $usuario, $senha, $porta, $dbname)
+    {
+        $this->host    = $host;
+        $this->usuario = $usuario;
+	$this->senha   = $senha;
+	$this->porta   = $porta;
+	$this->dbname  = $dbname;
+    }
+}
