9ea7bd04a5ca315a5ec063203b20eb7351e900e6
Thiago Lima <thiagolimaes@gmail.com>
2018-03-30 17:20:51 -0300

    Alt

--- src/SaperxPHP/Conector.php
+++ src/SaperxPHP/Conector.php
@@ -8,6 +8,11 @@ class Conector
     private $porta;
     private $dbname;
 
+    /**
+     * @var \PDO
+     */
+    private $pdo;
+
     public function __construct($host, $usuario, $senha, $porta, $dbname)
     {
         $this->host    = $host;
@@ -15,5 +20,27 @@ class Conector
         $this->senha   = $senha;
         $this->porta   = $porta;
         $this->dbname  = $dbname;
+
+        $this->connect();
+    }
+
+    public function __destruct()
+    {
+        $this->pdo = null;
+    }
+
+    private function connect()
+    {
+        $dsn  = "mysql:host={$this->host};port={$this->port};dbname={$this->dbname};charset=utf8";
+
+        try {
+            $this->pdo = new \PDO($dsn, $this->usuario, $this->senha, [
+                \PDO::ATTR_PERSISTENT         => false,
+                \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+            ]);
+        }
+        catch (\PDOException $e) {
+            throw new \Exception("SaperX database error.");
+        }
     }
 }
